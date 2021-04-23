19ee6934e2d6e344fc674c0a3fd11d3e5f97cdb5
AlejoHerrera <>
2021-04-23 05:33:20 +0000

    Circunferencia String

--- Java/Circunferencia.java
+++ Java/Circunferencia.java
@@ -5,29 +5,30 @@ import javax.swing.JFrame;
 public class Circunferencia extends JApplet {
 
   public void paint (Graphics g) {
-    int cx=200;
-    int cy=200;
-    g.drawString ("♠", cx, 350);
-    g.drawString ("♠", cx, 50);
-
-    int x=cx+1;
-    int y;
-
-    while (x <= 350){
-      y= x - 200;
-      g.drawString ("♠", x, y);
-      y= x + 200;
-      g.drawString ("♠", x, y);
-      x++;
-    }
-    x = 50;
-    while (x <= 200){
-      y= x + 150;
-      g.drawString ("♠", x, y);
-      y= x + 150;
-      g.drawString ("♠", x, y);
-      
-    }
+    g.setColor(Color.blue);
+    g.drawString ("###", 95,108);
+    g.drawString ("#", 85,118);
+    g.drawString ("#", 129,118);
+    g.drawString ("#", 75,130);
+    g.drawString ("#", 139,130);
+    g.drawString ("#", 70,142);
+    g.drawString ("#", 145,142);
+    g.drawString ("#", 70,152);
+    g.drawString ("#", 145,152);
+    g.drawString ("#", 70,162);
+    g.drawString ("#", 145,162);
+    g.drawString ("#", 70,172);
+    g.drawString ("#", 145,172);
+    g.drawString ("#", 75,185);
+    g.drawString ("#", 139,185);
+    g.drawString ("#", 85,197);
+    g.drawString ("#", 129,197);
+    g.drawString ("###", 95,207);
+    g.setColor(Color.black);
+    g.drawString ("Descarte la opción de usar",165,148);
+    g.drawString ("ciclos porque dieron la idea",165,162);
+    g.drawString ("en clase (⌣̩̩́_⌣̩̩̀)",165,176);
+    
   }
 
   public static void main (String[] args){
@@ -35,7 +36,7 @@ public class Circunferencia extends JApplet {
     Circunferencia circunferenciaApplet = new Circunferencia ();
     circunferenciaApplet.init ();
     frame.getContentPane ().add (circunferenciaApplet);
-    frame.setSize (400, 400);
+    frame.setSize (350, 350);
     frame.setVisible (true);
   }
 }
